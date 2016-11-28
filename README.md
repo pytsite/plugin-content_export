ad7eea2464d220ff9eff72837b44dd60d33b8655
Alexander Shepetko <a@shepetko.com>
2016-11-28 06:28:36 +0200

    Release 0.1.1

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Content export plugin for [PytSite](https://pytsite.xyz).
 
 ## Changelog
 
+### 0.1.1 (2016-11-28)
+- Fixed init.
+- Updated plugin.json.
+
 ### 0.1 (2016-11-28)
 - First release.
--- __init__.py
+++ __init__.py
@@ -5,37 +5,32 @@ from . import _model as model, _error as error
 from ._driver import Abstract as AbstractDriver
 from ._api import register_driver
 
+# Locally necessary imports
+from pytsite import admin as _admin, odm as _odm, events as _events, lang as _lang, router as _router, \
+    permissions as _permissions
+from . import _model, _eh
+
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+# Resources
+_lang.register_package(__name__)  # Required by odm_auth while model registration
+_lang.register_package(__name__, alias='content_export')
 
-def _init():
-    """Init wrapper.
-    """
-    from pytsite import admin, odm, events, lang, router, permissions
-    from . import _model, _eh
+# Permission group
+_permissions.define_group('content_export', 'content_export@content_export')
 
-    # Resources
-    lang.register_package(__name__)  # Required by odm_auth while model registration
-    lang.register_package(__name__, alias='content_export')
+# ODM models
+_odm.register_model('content_export', _model.ContentExport)
 
-    # Permission group
-    permissions.define_group('content_export', 'content_export@content_export')
+# Event handlers
+_events.listen('pytsite.cron.1min', _eh.cron_1min)
 
-    # ODM models
-    odm.register_model('content_export', _model.ContentExport)
-
-    # Event handlers
-    events.listen('pytsite.cron.1min', _eh.cron_1min)
-
-    # Admin sidebar menu
-    m = 'content_export'
-    admin.sidebar.add_menu(sid='content', mid=m, title=__name__ + '@export',
-                           href=router.ep_path('pytsite.odm_ui@browse', {'model': m}),
-                           icon='fa fa-bullhorn',
-                           permissions=('pytsite.odm_perm.view.' + m, 'pytsite.odm_perm.view_own.' + m),
-                           weight=100)
-
-
-_init()
+# Admin sidebar menu
+m = 'content_export'
+_admin.sidebar.add_menu(sid='content', mid=m, title=__name__ + '@export',
+                        href=_router.ep_path('pytsite.odm_ui@browse', {'model': m}),
+                        icon='fa fa-bullhorn',
+                        permissions=('pytsite.odm_perm.view.' + m, 'pytsite.odm_perm.view_own.' + m),
+                        weight=100)
